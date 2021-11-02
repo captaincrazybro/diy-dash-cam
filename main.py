d0ff6a87b95d1aba26cd720c070f61530d733d37
Wenger <lwenger1@highpoint.edu>
2021-11-02 14:11:07 -0400

    Project initialized

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from picamera import PiCamera
+from time import sleep
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    camera = PiCamera()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    camera.start_preview()
+    sleep(5)
+    camera.capture('/home/pi/Desktop/image.jpg')
+    camera.stop_preview()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
