37668f929cb1248eb149cea72115867c6b89bb12
Wenger <lwenger1@highpoint.edu>
2021-11-02 14:50:15 -0400

    Added some text

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
 
     camera.start_preview()
     camera.start_recording('/home/pi/Desktop/video.h264')
-    camera.annotate_text = "Hello ther!"
+    camera.annotate_text = "Time: 12:05, Velocity: 35 m/s, Temperature 35C"
     sleep(5)
     camera.stop_recording()
     camera.stop_preview()
