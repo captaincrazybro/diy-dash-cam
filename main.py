943768bc65f101534d1d2703aef6f099be0f7aad
Wenger <lwenger1@highpoint.edu>
2021-11-02 14:22:21 -0400

    Removed text

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     camera = PiCamera()
 
     camera.start_preview()
+    camera.annotate_text = ""
     camera.start_recording('/home/pi/Desktop/video.h264')
     sleep(5)
     camera.stop_recording()
