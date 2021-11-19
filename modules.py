68b3b3cc1f125dee10706ff64a0d8d4258f910b7
captaincrazybro <myeyesareonmizar@gmail.com>
2021-11-18 20:08:39 -0500

    Changed syntax error

--- modules.py
+++ modules.py
@@ -8,16 +8,16 @@ def recording_icon(sense, is_recording):
 
     if not on_off or not is_recording:
         off = (0, 0, 0)
-        sense.set(1, 1, off)
-        sense.set(1, 2, off)
-        sense.set(2, 1, off)
-        sense.set(2, 2, off)
+        sense.set_pixel(1, 1, off)
+        sense.set_pixel(1, 2, off)
+        sense.set_pixel(2, 1, off)
+        sense.set_pixel(2, 2, off)
     else:
         red = (255, 0, 0)
-        sense.set(1, 1, red)
-        sense.set(1, 2, red)
-        sense.set(2, 1, red)
-        sense.set(2, 2, red)
+        sense.set_pixel(1, 1, red)
+        sense.set_pixel(1, 2, red)
+        sense.set_pixel(2, 1, red)
+        sense.set_pixel(2, 2, red)
 
     on_off = not on_off
 
