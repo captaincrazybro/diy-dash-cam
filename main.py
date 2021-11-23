f8cf065f654e6f0b3221a7bae00c5826bef00bfb
captaincrazybro <myeyesareonmizar@gmail.com>
2021-11-22 20:36:24 -0500

    Fixed bug

--- main.py
+++ main.py
@@ -23,66 +23,67 @@ def main():
     overheating = False
     recording = True
     video_count = utils.get_count()
-    global_time = 1
+    global_time = 0
     t = 1
 
-    # camera.annotate_text = display_details(global_time, 13, convert_temp(sense.get_temperature()))
+    camera.annotate_text = display_details(global_time, 13, convert_temp(sense.get_temperature()))
     camera.start_recording(f'{recordings_home}/recording-{video_count}.h264')
 
     while True:
 
+        global_time += 1
         sleep(1)
 
-        # # Checks if raspberry pi is overheating
-        # if get_cpu_temperature() >= max_temp:
-        #     if not overheating:
-        #         sense.clear()
-        #         camera.stop_recording()
-        #         overheating = True
-        #         video_count = add_count(video_count)
-        #     utils.show_temp_warning(sense)
-        #     continue
-        # elif overheating:
-        #     utils.show_check(sense)
-        #     sleep(1)
-        #     sense.clear()
-        #     sleep(1)
-        #
-        #     space_manager()
-        #
-        #     camera.start_recording(f'/home/pi/Desktop/Recordings/recording-{video_count}.h264')
-        #     t = 0
-        #     overheating = False
+        # Checks if raspberry pi is overheating
+        if get_cpu_temperature() >= max_temp:
+            if not overheating:
+                sense.clear()
+                camera.stop_recording()
+                overheating = True
+                video_count = add_count(video_count)
+            utils.show_temp_warning(sense)
+            continue
+        elif overheating:
+            utils.show_check(sense)
+            sleep(1)
+            sense.clear()
+            sleep(1)
 
-        # # Handle button press event
-        # handled_button = handle_button(sense)
-        # if handled_button is 1:
-        #     if recording:
-        #         recording = False
-        #         camera.stop_recording()
-        #         video_count = add_count(video_count)
-        #         space_manager()
-        #     else:
-        #         recording = True
-        #         camera.start_recording(f'/home/pi/Desktop/Recordings/recording-{video_count}.h264')
-        #         t = 0
-        # if handled_button > 1:
-        #     recording_icon(sense, False)
-        #     camera.stop_recording()
-        #     video_count = add_count(video_count)
-        #
-        #     utils.transfer_files(transfer_all=True if handled_button is 3 else False)
-        #
-        #     space_manager()
-        #     camera.start_recording(f'/home/pi/Desktop/Recordings/recording-{video_count}.h264')
-        #     t = 0
-        #     sleep(1)
+            space_manager()
 
-        # print(recording, global_time)
+            camera.start_recording(f'/home/pi/Desktop/Recordings/recording-{video_count}.h264')
+            t = 0
+            overheating = False
+
+        # Handle button press event
+        handled_button = handle_button(sense)
+        if handled_button is 1:
+            if recording:
+                recording = False
+                camera.stop_recording()
+                video_count = add_count(video_count)
+                space_manager()
+            else:
+                recording = True
+                camera.start_recording(f'/home/pi/Desktop/Recordings/recording-{video_count}.h264')
+                t = 0
+        if handled_button > 1:
+            recording_icon(sense, False)
+            camera.stop_recording()
+            video_count = add_count(video_count)
+
+            utils.transfer_files(transfer_all=True if handled_button is 3 else False)
+
+            space_manager()
+            camera.start_recording(f'/home/pi/Desktop/Recordings/recording-{video_count}.h264')
+            t = 0
+            sleep(1)
+
+        print(handled_button, recording, global_time)
 
         # LED Grid recording blink
-        # recording_icon(sense, recording)
-        # show_storage_usage(sense)
+        recording_icon(sense, recording)
+        show_storage_usage(sense)
 
         # Check if camera is supposed to be recording
         if not recording:
@@ -98,9 +99,8 @@ def main():
             t = 0
 
         # Update display text (showing time, speed and temperature)
-        # camera.annotate_text = display_details(global_time, 13, convert_temp(sense.get_temperature()))
+        camera.annotate_text = display_details(global_time, 13, convert_temp(sense.get_temperature()))
 
-        global_time += 1
         t += 1
 
 
