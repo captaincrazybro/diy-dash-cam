2308abd26be4865cf8ae60125055d6e1fb49a65a
Wenger <lwenger1@highpoint.edu>
2021-11-30 13:27:29 -0500

    Fixed a bug

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
     t = 1
 
     gpsp.start()
-    camera.annotate_background = camera.Color('black')
+    camera.annotate_background = picamera.Color('black')
     camera.annotate_text = display_details(convert_temp(sense.get_temperature()))
     camera.start_recording(f'{recordings_home}/recording-{video_count}.h264')
 
