db3796c4c9f347883762cd209f0335a5d0fb08ad
captaincrazybro <myeyesareonmizar@gmail.com>
2021-12-07 20:06:42 -0500

    QOL changes

--- utils.py
+++ utils.py
@@ -45,7 +45,7 @@ def gpsd_is_set():
 
 
 def display_details(temperature):
-    print(gpsd if gpsd is None else gpsd.fix.speed)
+    print(gpsd if gpsd is None else gpsd.fix.speed + "           ")
     if not gpsd_is_set():
         return 'Obtaining GPS...'
 
