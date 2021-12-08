5de3b343f1308665b054f7c971e0af2750dcca0f
captaincrazybro <myeyesareonmizar@gmail.com>
2021-12-08 16:28:52 -0500

    Re added implementation

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def main():
     camera.rotation = 90
 
     gpsp.start()
-    # gpsEnhancer.start()
+    gpsEnhancer.start()
     camera.annotate_background = picamera.Color('black')
     camera.annotate_text = display_details(convert_temp(sense.get_temperature_from_pressure()))
     # camera.start_preview()
